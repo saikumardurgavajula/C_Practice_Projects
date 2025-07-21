1e0f7c87fb629ba8530e9e0b667a729fca153e7f
saikumardurgavajula <22341A0230@gmrit.edu.in>
2025-07-21 15:29:40 +0530

    Second program

---
+++ add.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a;
+    printf("Enter the first value:");
+    scanf("%d",&a);
+    int b;
+    printf("Enter the second value:");
+    scanf("%d",&b);
+    int c=a+b;
+    printf("sum is:%d",c);
+    return 0;
+
+}
